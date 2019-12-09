b633e79f1a0580d237ff2384a8eb6de64084deac
Jafar <rohmadijafar@gmail.com>
2019-12-10 00:05:59 +0700

    'update'

--- app/Http/Requests/StoreSettingRequest.php
+++ app/Http/Requests/StoreSettingRequest.php
@@ -11,7 +11,7 @@ class StoreSettingRequest extends FormRequest
 {
     public function authorize()
     {
-        abort_if(Gate::denies('setting_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('setting_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         return true;
     }
