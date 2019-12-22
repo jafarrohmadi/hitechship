f2c0b55d7cab2824dcac7b52666ae07425f30979
jafar0987 <rohmadijafar@gmail.com>
2019-12-22 22:53:20 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,6 +30,7 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
         $schedule->command('getShip:data')->hourly();
         $schedule->command('getHistoryShip:data')->hourly();
+        $schedule->command('queue:work --stop-when-empty')->hourly()->withoutOverlapping();
     }
 
     /**
